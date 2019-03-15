a8dd918299be058e1649e5f098b36d2a938b0b1d
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-15 04:53:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr = []
+  for el in array do
+    arr.push("Hello, my name is #{el}.")
+  end
+  arr
+end
+
+def assign_rooms(array)
+  array.each_with_index do { |el, i|
+    puts "Hello, #{el}! You'll be assigned to room #{i}!"
+  }
+end
+
+def printer(array)
+  batch_badge_creator(array)
+  assign_rooms(array)
+end
